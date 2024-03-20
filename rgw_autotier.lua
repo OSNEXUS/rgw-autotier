cad0800c8d8070666f021a6b7fd3022e1fefb485
Steven Umbehocker <steven.umbehocker@osnexus.com>
2024-03-20 04:39:39 +0000

    adds support for autotiering by bucket name and tenant name

--- rgw_autotier.lua
+++ rgw_autotier.lua
@@ -1,7 +1,6 @@
 --
 -- S3 Object Auto-tiering Script for CephRGW
 -- Copyright (c) 2024 OSNexus <info@osnexus.com>
---
 -- Author: Steven Umbehocker
 --
 --  This library is free software; you can redistribute it and/or
@@ -10,6 +9,11 @@
 --  version 2.1 of the License, or (at your option) any later version.
 --
 --
+
+-- see https://docs.ceph.com/en/quincy/radosgw/lua-scripting/#request-fields for full 
+-- list of Request fields and writable options.
+
+-- see https://github.com/OSNEXUS/rgw-autotier for GitHub documentation on this script
 
 patternsFilename = "/etc/ceph/rgw_autotier.prop"
 
@@ -33,9 +37,27 @@ local function isEmpty(val)
   return val == nil or val == ''
 end
 
--- exit early if this is not a S3 PUT operation
-if Request.RGWOp ~= 'put_obj' then
-  -- RGWDebugLog("Not a S3 PUT operation, skipping auto-tiering.")
+local function trim(val)
+  return val:match( "^%s*(.-)%s*$" )
+end
+
+local function split(val)
+  local result = {}
+  local i = 1;
+  for token in string.gmatch(val, "[^&;]+") do
+    result[i] = trim(token)
+    i = i+1
+  end
+  return result
+end
+
+local function startsWith(val, pat)
+    return val:sub(1, #pat) == pat
+end
+
+-- exit early if this is not a S3 PUT or multipart upload operation
+if Request.RGWOp ~= 'put_obj' and Request.RGWOp ~= 'complete_multipart' and Request.RGWOp ~= 'init_multipart' then
+  -- RGWDebugLog("Not a auto-tierable S3 operation skipping auto-tiering: '" .. Request.RGWOp .. "' object " .. Request.Object.Name)
   return
 end
 
@@ -60,33 +82,57 @@ end
 patternsFile = io.open(patternsFilename, "r")
 for line in patternsFile:lines() do
 
+  tokens = {}
+  tokens = split(line)
+
   -- RGWDebugLog("Processing storage class match rule: " .. line)
+  if tokens ~= nil and #tokens >= 2 then
+    
+    storageClass = tokens[1]
 
-  for storageClass,patternMatch,operator,capacityThreshold in string.gmatch(line, "([^&;]+);([^&;]+);([^&;]+);([^&;]+)") do 
-
-    -- check for valid storage class
-    if storageClass == nil then 
+    -- check if comment line
+    if storageClass == '' or startsWith(storageClass, '--') or startsWith(storageClass, '#') then
       goto continue_gmatch
     end
+    
+    patternMatch = tokens[2]
+    capacityThreshold = "0"
+    operator = ""
+    bucketMatch = ""
+    tenantMatch = ""
 
-    -- skip if empty or a comment line
-    storageClass = string.gsub(storageClass, "%s+", "")
-    if storageClass == '' or storageClass == '--' or storageClass == '#' then
-      goto continue_gmatch
+    -- check for valid object size / content length matching params
+    if #tokens >= 3 then
+        operator = tokens[3]
+        if #tokens >= 4 then
+            capacityThreshold = tokens[4]
+        end
     end
 
-    -- RGWDebugLog("Processing tokens: storageClass '" .. storageClass .. "' patternMatch '" .. patternMatch .. "' operator '" .. operator .. "' capacityThreshold '" .. capacityThreshold .. "'")
+    -- check for valid bucket specifier
+    if #tokens >= 5 then
+        bucketMatch = tokens[5]
+    end
+    
+    -- check for valid tenant specifier
+    if #tokens >= 6 then
+        tenantMatch = tokens[6]
+    end
+
+    -- RGWDebugLog("Processing tokens: storageClass '" .. storageClass .. "' patternMatch '" .. patternMatch .. "' operator '" .. operator .. "' capacityThreshold '" .. capacityThreshold .. "' bucketMatch '" .. bucketMatch .. "' tenantMatch '" .. tenantMatch .. "'")
 
     -- check for match to capacity threshold
     if operator ~= nil and capacityThreshold ~= nil then
-      operator = string.gsub(operator, "%s+", "")
-      capacityThreshold = string.gsub(capacityThreshold, "%s+", "")
       if isInteger(capacityThreshold) then
         capacity = tonumber(capacityThreshold)
         if capacity > 0 then
-          if operator == '>' and Request.ContentLength < capacity then
+          if operator == '>' and Request.ContentLength <= capacity then
             goto continue_gmatch
-          elseif operator == '<' and Request.ContentLength > capacity then
+          elseif operator == '<' and Request.ContentLength >= capacity then
+            goto continue_gmatch
+          elseif operator == '>=' and Request.ContentLength < capacity then
+            goto continue_gmatch
+          elseif operator == '<=' and Request.ContentLength > capacity then
             goto continue_gmatch
           elseif operator == '=' and Request.ContentLength ~= capacity then
             goto continue_gmatch
@@ -95,15 +141,29 @@ for line in patternsFile:lines() do
       end
     end
 
-    -- check for match to file name pattern
-    patternMatch = string.gsub(patternMatch, "%s+", "")
+    -- check for exact match to bucket name
+    if bucketMatch ~= nil and bucketMatch ~= "" then
+        if bucketMatch ~= Request.Bucket.Name then
+            goto continue_gmatch
+        end
+    end
+
+    -- check for exact match to tenant name
+    if tenantMatch ~= nil and tenantMatch ~= "" then
+        if tenantMatch ~= Request.Bucket.Tenant then
+            goto continue_gmatch
+        end
+    end
+
+    -- check for match to object name
     if patternMatch ~= nil and (patternMatch == "*" or string.find(Request.Object.Name, patternMatch)) then
       Request.HTTP.StorageClass = storageClass
-      RGWDebugLog("  Object " .. Request.Object.Name .. " matched: storageClass '" .. storageClass .. "' patternMatch '" .. patternMatch .. "' operator '" .. operator .. "' capacityThreshold '" .. capacityThreshold .. "'")
+      RGWDebugLog("  Object " .. Request.Object.Name .. " matched: storageClass '" .. storageClass .. "' patternMatch '" .. patternMatch .. "' operator '" .. operator .. "' capacityThreshold '" .. capacityThreshold .. "' bucketMatch '" .. bucketMatch .. "' tenantMatch '" .. tenantMatch .. "'")
       return
     end
 
     ::continue_gmatch::
+
   end
 end
 
